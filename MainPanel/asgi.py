28b0c168a691b41441315387d239ff76d6c081e8
yazdanimehdi <my_jahromi@yahoo.com>
2020-08-09 16:04:25 +0430

    channels added for transaction

--- MainPanel/asgi.py
+++ MainPanel/asgi.py
@@ -10,7 +10,8 @@ https://docs.djangoproject.com/en/3.0/howto/deployment/asgi/
 import os
 
 from channels.routing import get_default_application
+import django
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'MainPanel.settings')
-
+django.setup()
 application = get_default_application()
